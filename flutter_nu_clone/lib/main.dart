ad2adbc00256a6469f41c2d650e2867978778159
Gustavo <gustavo.tatarem@saedigital.com.br>
2019-12-30 11:15:16 -0300

    Setup for debugging

--- flutter_nu_clone/lib/main.dart
+++ flutter_nu_clone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_nu_clone/pages/home_page.dart';
 import 'package:flutter_nu_clone/pages/login_page.dart';
 import 'package:flutter_nu_clone/services/service_locator.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.purple,
       ),
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
